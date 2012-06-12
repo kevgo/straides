53054d1728459694ba17a38c0a5ee1fed3ba1b4e
Kevin Goslar <kevin.goslar@originate.com>
2012-06-12 12:46:57 -0700

    Makes Straides work in unit tests.

--- lib/straides/railtie.rb
+++ lib/straides/railtie.rb
@@ -7,7 +7,8 @@ module Straides
     # Initialize Straides after the Rails initializers have run.
     config.after_initialize do
       if Straides::Configuration.instance.auto_load
-        ActionController::Base.send :include, Straides
+        # Note (KG): Including Straides into ActionController::Base here doesn't work in unit tests.
+        ApplicationController.send :include, Straides
       end
     end
   end
