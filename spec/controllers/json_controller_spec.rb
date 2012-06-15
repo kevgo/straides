98cb563f2ddc32e9b191d3f87a583d5bb095b975
Kevin Goslar <kevin.goslar@originate.com>
2012-06-14 19:34:24 -0700

    Ruby 1.8.7 compatibility.

--- spec/controllers/json_controller_spec.rb
+++ spec/controllers/json_controller_spec.rb
@@ -5,7 +5,7 @@ describe JsonController do
 
   describe "error code" do
     it 'returns the given error code' do
-      get :code, format: 'json'
+      get :code, :format => 'json'
       response.status.should == 401
     end
   end
@@ -14,21 +14,21 @@ describe JsonController do
 
     describe 'default behavior' do
       it 'renders an empty response' do
-        get :code, format: 'json'
+        get :code, :format => 'json'
         response.body.strip.should == ''
       end
     end
 
     context ':text given' do
       it 'renders the given string as the response text' do
-        get :text, format: 'json'
+        get :text, :format => 'json'
         response.body.should == 'descriptive error message'
       end
     end
 
     context ':nothing given' do
       it 'renders an empty response' do
-        get :nothing, format: 'json'
+        get :nothing, :format => 'json'
         response.body.strip.should == ''
       end
     end
