23ba33dfe3b3e61d5aeb9ae4d549e4c558e61ab3
Kevin Goslar <kevin.goslar@originate.com>
2013-04-17 15:18:38 -0700

    Cleanup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,9 +16,5 @@ ENV["RAILS_ENV"] = "test"
 
 RSpec.configure do |config|
   config.mock_with :rspec
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-#  config.use_transactional_fixtures = true
+  #  config.use_transactional_fixtures = true
 end
